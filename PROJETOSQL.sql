f53631a1b6bf15cdd38624be6651c40ac30f4627
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-12-27 01:08:52 -0300

    Add files via upload

---
+++ PROJETOSQL.sql
@@ -0,0 +1,12 @@
+CREATE database LIVRARIA ;
+
+create table LIVROS(
+	LIVRO varchar (100),
+    AUTOR varchar (100),
+    SEXO char (1),
+    PAGINAS int (5),
+    EDITORA varchar(30),
+    VALOR float (10,2),
+    UF char(2),
+    ANO int(5)
+    );
